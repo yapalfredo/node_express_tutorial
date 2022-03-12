573502118a9da576e277038e334f6755bf48d8a2
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-03-11 22:57:51 -0500

    done with Async patterns - native option

--- app.js
+++ app.js
@@ -15,9 +15,42 @@
 // npm init -y (everything default)
 
 
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems)
 
+// const getText = (path) => {
+//     return new Promise ((resolve, reject) => {
+//         readFile(path, 'utf8', (err,data) => {
+//             if (err){
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+const {readFile, writeFile} = require('fs').promises
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/test2.txt', 'utf8')
+        const second = await readFile('./content/subfolder/test.txt', 'utf8')
+        await writeFile('./content/result-mind-buggling.txt', `THIS IS JUST SAMPLE: ${first} ${second}`,
+         {flag: 'a'})
+
+        console.log(first)
+        console.log(second)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
